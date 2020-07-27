b5ee89d6f8b8d4e35490ae5b3e18f352d93766ff
bakti <bakti.widianto@gmail.com>
2020-07-28 04:09:32 +0700

    create router chat

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -5,10 +5,10 @@ var chatSchema = new mongoose.Schema(
         chatId: Number,
         name: String,
         message: String
-    },
-    {
-        timestamps: true
     }
+    // {
+    //     timestamps: true
+    // }
 );
 
 module.exports = mongoose.model('Chat', chatSchema);
--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,13 +1,46 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
 var Chat = require('../models/Chat')
 
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  Chat.find()
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
+  Chat.find().sort({ createdAt: 1 })
+    .then(result => {
+      let data = result.map(item => {
+        return {
+          _id: item._id,
+          id: item.chatId,
+          name: item.name,
+          message: item.message,
+          date: moment(item.createdAt).format("YYYY-MM-DD"),
+          time: moment(item.createdAt).format("HH:mm:ss")
+        }
+      })
+      res.json({
+        error: false,
+        data: data
+      })
+    })
+    .catch(err => res.json(err))
+});
+
+
+router.post('/', function (req, res, next) {
+  Chat.create({ chatId: req.body.id, name: req.body.name, message: req.body.message })
+    .then(result => {
+      res.json({
+        error: false,
+        chat: result
+      })
+    })
+    .catch(err => {
+      res.json({
+        error: true,
+        message: err
+      })
+    })
 });
 
 module.exports = router;
